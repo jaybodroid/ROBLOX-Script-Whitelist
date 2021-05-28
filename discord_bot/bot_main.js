504e76e19aa73140bd40260b4e2c0f38b45fb155
PnyxDev <74435895+PnyxDev@users.noreply.github.com>
2021-05-28 16:50:18 -0400

    Create bot_main.js

---
+++ discord_bot/bot_main.js
@@ -0,0 +1,80 @@
+/*
+
+
+██████   ██████  ████████      ███    ███  █████  ██ ███    ██         ██ ███████ 
+██   ██ ██    ██    ██         ████  ████ ██   ██ ██ ████   ██         ██ ██      
+██████  ██    ██    ██         ██ ████ ██ ███████ ██ ██ ██  ██         ██ ███████ 
+██   ██ ██    ██    ██         ██  ██  ██ ██   ██ ██ ██  ██ ██    ██   ██      ██ 
+██████   ██████     ██ ███████ ██      ██ ██   ██ ██ ██   ████ ██  █████  ███████ 
+                                                                                  
+          ██████  ██    ██     ██████  ███    ██ ██    ██ ██   ██ 
+          ██   ██  ██  ██      ██   ██ ████   ██  ██  ██   ██ ██  
+          ██████    ████       ██████  ██ ██  ██   ████     ███   
+          ██   ██    ██        ██      ██  ██ ██    ██     ██ ██  
+          ██████     ██        ██      ██   ████    ██    ██   ██ 
+                                      
+*/
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ext = require("./external");
+
+const config = ext.loadConfig();
+const functions = ext.loadFunctions();
+ext.loadCommands();
+
+client.on("message", async (message) => {
+    if (message.content.startsWith(config.prefix)) {
+        var processedCommand = await functions.parseCommand(message);
+        if (processedCommand !== null) {
+            if (processedCommand.command.arguments == processedCommand.provided_args.length) {
+                if (processedCommand.canRun) {
+                    try {
+                        processedCommand.command.function(message, processedCommand.provided_args);
+                    } catch(Error) {
+                        if (Error.indexOf("yourwebsite.com") == -1) {
+                            functions.sendResponseEmbed(
+                                client.channels.get(config.logs.errors.id),
+                                "commandfailure",
+                                `An error has occurred whilst <@${message.author.id}> was running the command "${processedCommand["name"]}".\n` + "```" + Error + "```",
+                                "Error id: " + functions.createId(),
+                            )
+                        } else {
+                            functions.sendResponseEmbed(
+                                client.channels.get(config.logs.errors.id),
+                                "commandfailure",
+                                `An error has occurred whilst <@${message.author.id}> was running the command "${processedCommand["name"]}".\n` + "```" + "Error information has been blocked due to it possibly containing sensitive information." + "```",
+                                "Error id: " + functions.createId(),
+                            )
+                        }
+                    }
+                } else {
+                    functions.sendResponseEmbed(
+                        message.channel,
+                        "commandfailure",
+                        "Insufficient permissions.",
+                        "Command ID: " + functions.createId()
+                    )
+                }
+            } else {
+                functions.sendResponseEmbed(
+                    message.channel,
+                    "commandfailure",
+                    "An invalid amount of arguments have been provided, try again.",
+                    "Command ID: " + functions.createId()
+                )
+            }
+        } else {
+            functions.sendResponseEmbed(
+                message.channel,
+                "commandfailure",
+                "Command not found, a list of commands can be found by using the help command.",
+                "Command ID: " + functions.createId()
+            )
+        }
+    }
+});
+
+client.login("YOUR_BOT_TOKEN");
+console.clear();
+console.info("Activated.")
